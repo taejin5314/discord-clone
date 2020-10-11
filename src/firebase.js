d932c9666f9368022fb915678ebeeb2601e29598
taejin <taejin5314@gmail.com>
2020-10-11 17:12:36 -0400

    Initialize the project by using npx create-react-app

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB1kUVdgyKipomHMq008zXbjzsWR4cM2oE",
+    authDomain: "discord-clone-b40ec.firebaseapp.com",
+    databaseURL: "https://discord-clone-b40ec.firebaseio.com",
+    projectId: "discord-clone-b40ec",
+    storageBucket: "discord-clone-b40ec.appspot.com",
+    messagingSenderId: "879806809761",
+    appId: "1:879806809761:web:8a8957a646845e824692e0",
+    measurementId: "G-7B93BD4LTK"
+};
