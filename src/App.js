ab3aea946d8176e2a5329a499eab45a41253688f
taejin <taejin5314@gmail.com>
2020-10-11 17:53:09 -0400

    Creating components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build a discord clone</h1>
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Chat */}
     </div>
   );
 }
