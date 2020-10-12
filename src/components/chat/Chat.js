65ad520b247d63a0757bb459ae08aecc3842a4dc
taejin <taejin5314@gmail.com>
2020-10-12 18:58:38 -0400

    Update Chat.js

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Chat.css'
+import ChatHeader from './chatHeader/ChatHeader';
 
 function Chat() {
     return (
         <div className='chat'>
-
+            <ChatHeader />
         </div>
     )
 }
